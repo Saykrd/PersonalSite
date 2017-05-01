a8e397f3857375e385185ea8f0130a9647f4a2e4
Saykrd <sacredrealm88@gmail.com>
2017-05-01 00:37:27 -0400

    All buttons work as they should

--- bin/js/main.js
+++ bin/js/main.js
@@ -3,3 +3,34 @@
 var navBarControls = {
 
 }
+
+function onScrollLink(e){
+	e.preventDefault();
+	var sectionID = $(this).attr("data-id");
+	console.log(this, sectionID);
+	scrollToID('#' + sectionID, 750);
+}
+
+function onScrollTop(e){
+	e.preventDefault();
+	$('html, body').animate({scrollTop: 0}, 'slow');
+}
+
+
+function scrollToID(id, speed){
+	var offset = 50;
+	var targetOffset = $(id).offset().top - offset;
+	var mainNav = $('#main-nav');
+	$('html,body').animate({scrollTop:targetOffset}, speed);
+	if (mainNav.hasClass("open")) {
+		mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+		mainNav.removeClass("open");
+	}
+}
+
+$(document).ready(
+	function() { 
+		$('.scroll-link').on('click', onScrollLink);
+		$('.scroll-top').on('click', onScrollTop);
+	}
+)

831a944110c4104d0daa2af208c1f780aca227c3
Saykrd <sacredrealm88@gmail.com>
2017-05-04 12:06:08 -0400

    Canvas now resizes properly

--- js/introbanner.js
+++ js/introbanner.js
@@ -1,81 +1,131 @@
 
+var banner = function(){
+	
+}
 
-$(document).ready(
-	function() {
-		console.log("ready 1");
-
+banner.prototype = {
+	init : function(){
 		var width = $(window).width();
 		var height = $(window).height();
 
 		console.log(width, height);
 
-		var app = new PIXI.Application(width, height, {transparent : true});
+		this.app = new PIXI.Application(width, height, {transparent : true});
 		//app.renderer.backgroundColor = 0xFFFFFF;
 
-		app.view.style.position = 'absolute';
-		app.view.style.top = '0';
-		app.view.style.left = '0';
-		document.body.appendChild(app.view);
+		this.app.view.style.position = 'absolute';
+		this.app.view.style.top = '0';
+		this.app.view.style.left = '0';
+		document.body.appendChild(this.app.view);
 
-		var texture = PIXI.Texture.fromImage('media/img/texture/bg_intro.jpg');
-		var texture2= PIXI.Texture.fromImage('media/img/texture/checker_plus.png');
+		this.build();
+	},
+
+	build : function(){
+		console.log("build");
+
+		var width = $(window).width();
+		var height = $(window).height();
+
+		this.app.renderer.resize(width, height);
+
+		if(!this.texture) this.texture = PIXI.Texture.fromImage('media/img/texture/bg_intro.jpg');
+		if(!this.texture2) this.texture2= PIXI.Texture.fromImage('media/img/texture/checker_plus.png');
 		
 		//var sprite = new PIXI.Sprite(texture);
 
-		var bg = new PIXI.Graphics();
-		bg.beginFill(0x53CBF4);
-		bg.drawRect(0,0, width, height);
-		bg.endFill();
+		this.bg = new PIXI.Graphics();
+		this.bg.beginFill(0x53CBF4);
+		this.bg.drawRect(0,0, width, height);
+		this.bg.endFill();
 
-		var container = new PIXI.Container();
+		this.container = new PIXI.Container();
 
 		//sprite.scale.x = 2;
 		//sprite.scale.y = 2;
 
-		app.stage.addChild(bg);
-		container.scale.set(2,2);
+		this.app.stage.addChild(this.bg);
+		this.container.scale.set(2,2);
+
+		this.bgTiles = [];
 
 		for (var i = 0; i < Math.ceil(width / 512); i++) {
 			for (var j = 0; j < Math.ceil(height / 512); j++) {
-				var overlay = new PIXI.Sprite(texture2);
+				var overlay = new PIXI.Sprite(this.texture2);
 				overlay.x = i * 512;
 				overlay.y = j * 512;
-				container.addChild(overlay)
+
+				this.bgTiles.push(overlay);
+				this.container.addChild(overlay)
 			};
 		};
 
-		app.stage.addChild(container);
+		this.app.stage.addChild(this.container);
 
 		
 		
 
-		var shockwave = new PIXI.filters.ShockwaveFilter();
+		this.shockwave = new PIXI.filters.ShockwaveFilter();
 		//shockwave.size.set(4,4);
-		shockwave.time = 0;
+		this.shockwave.time = 0;
 
-		var bloom = new PIXI.filters.BloomFilter();
-		bloom.blur = 5;
+		this.bloom = new PIXI.filters.BloomFilter();
+		this.bloom.blur = 5;
 
-		container.filters = [shockwave];
+		this.container.filters = [this.shockwave];
 
 
 
-		var timeline1 = new TimelineMax({});
-		var timeline2 = new TimelineMax({});
+		this.timeline1 = new TimelineMax({});
+		this.timeline2 = new TimelineMax({});
 
 		//timeline.to(sprite.scale, 15,  {"x" : 2, "y" : 2, "repeat" : -1, "yoyo" : true, "ease" :  Linear.easeInOut});
-		timeline1.add(TweenMax.to(shockwave, 5, {time : 1, repeat : -1}))
-		timeline2.add(TweenMax.to(container, 20,  {"x" : "-=500", "y" : "-=500", "repeat" : -1, "yoyo" : true, "ease" :  Linear.easeInOut}));
+		this.timeline1.add(TweenMax.to(this.shockwave, 5, {time : 1, repeat : -1}))
+		this.timeline2.add(TweenMax.to(this.container, 20,  {"x" : "-=500", "y" : "-=500", "repeat" : -1, "yoyo" : true, "ease" :  Linear.easeInOut}));
 		
+	},
+
+	deconstruct : function(){
+		console.log("deconstruct");
+
+		TweenMax.killAll();
+
+		this.timeline1 = this.timeline2 = null;
+
+		this.bgTiles.forEach( function(element, index) {
+			element.destroy();
+		});
+
+		this.bgTiles.length = 0;
+
+		this.container.removeChildren();
+		this.container.destroy();
+		this.bg.destroy();
+
+		this.container = null;
+		this.bg = null;
+
+		this.shockwave = this.bloom = null;
+	},
+
+	resize :  function(){
+		console.log("resize");
+		this.deconstruct();
+		this.build();
+	}
+}
+
+$(document).ready(
+	function() {
+		console.log("ready");
+
+		var b = new banner();
+		b.init();
 
 		$(window).resize(
 			function(event){
-				console.log(event);
-				var width = $(window).width();
-				var height = $(window).height();
-				app.renderer.resize(width, height)
+				b.resize();
 			}
 		)
-
 	}
 )
